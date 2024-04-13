ed9ab7a8e43bd99782a692a4266458ac71d026c3
Wyz Kim-Chan <kimblader151@yahoo.com.sg>
2024-04-13 23:03:18 +0800

    Create subclass of QMainWindow for organization and add a button

--- application_files/app.py
+++ application_files/app.py
@@ -3,14 +3,25 @@ from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 # Only needed for access to command line arguments
 import sys
 
+
+class MainWindow(QMainWindow):
+    """Subclass the QMainWindow to customize your application's main window"""
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My Application")
+        button = QPushButton("Click me!")
+
+        # Set the central widget of the window
+        self.setCentralWidget(button)
+
+
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 # If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
 
-# Create a Qt Main Window
-window = QMainWindow()
-
+# Create a Main Window
+window = MainWindow()
 # Windows are hidden by default - so you must use this to show the window
 window.show()
 
